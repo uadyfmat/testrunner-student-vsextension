d1183d0b75ed2ec16fca4e426ca007d475e71b81
EstebanMadrazo <3steban.madrazo@gmail.com>
2024-05-02 00:12:25 -0600

    Se reparó el botón de instalación

--- Test-Runner-Extension/extension.js
+++ Test-Runner-Extension/extension.js
@@ -5,22 +5,7 @@ const { exec } = require('child_process');
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-class TestRunnerDataProvider {
-    constructor() {
-        this.button = new vscode.TreeItem('Install Test-Runner');
-        this.button.command = 'extension.installTestRunner'; // Asigna el comando al botón
-    }
 
-    // Devuelve el botón como único elemento en la vista
-    getChildren() {
-        return [this.button];
-    }
-
-    // Devuelve el elemento como TreeItem
-    getTreeItem(element) {
-        return element;
-    }
-}
 
 function activate(context) {
    // Registra el comando para instalar Test-Runner
@@ -40,15 +25,6 @@ function activate(context) {
     });
 
     context.subscriptions.push(disposable);
-    // Crea una instancia del proveedor de datos
-    const dataProvider = new TestRunnerDataProvider();
-
-    // Registra el proveedor de datos en la vista 'package-install-test-runner'
-    const view = vscode.window.createTreeView('package-install-test-runner', {
-        treeDataProvider: dataProvider
-    });
-
-    context.subscriptions.push(view);
 }
 
 function deactivate() {}
