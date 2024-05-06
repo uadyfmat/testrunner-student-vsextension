dca812d4681d6182df7deeee8cb2f05bb677e0dc
TeoDawn <teodawn.td@gmail.com>
2024-05-06 13:51:04 -0600

    feat: Files to execute view of tests and simulate test execution

---
+++ Test-Runner-Extension/src/TestFile.js
@@ -0,0 +1,30 @@
+class TestFile {
+    constructor() {
+        this.tests = [];
+    }
+    
+    search() {
+        this.tests = [
+            {
+                name: 'helloTest',
+                description: 'Hello Test',
+                input: 'Hello',
+                output: 'Hello'
+            },
+            {
+                name: 'helloTest2',
+                description: 'Hello Test 2',
+                input: 'Bye',
+                output: 'Bye'
+            }
+        ]
+
+        return this.tests;
+    }
+    
+    tests() {
+        return this.tests;
+    }
+}
+
+module.exports = TestFile;
