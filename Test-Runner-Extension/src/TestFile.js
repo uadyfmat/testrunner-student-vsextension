d4e5f3cf1f8523bfb0acc2498297c342a6af5c8a
TeoDawn <teodawn.td@gmail.com>
2024-08-20 15:12:41 -0600

    fix error of test file imports

---
+++ Test-Runner-Extension/src/TestFile.js
@@ -0,0 +1,33 @@
+const vscode = require('vscode');
+const findSpecFiles = require('./test-finder');
+
+class TestFile {
+    constructor() {
+        this.tests = [];
+    }
+    
+    search() {
+        this.tests = [
+            {
+                name: 'helloTest',
+                description: 'Hello Test',
+                input: 'Hello',
+                output: 'Hello'
+            },
+            {
+                name: 'helloTest2',
+                description: 'Hello Test 2',
+                input: 'Bye',
+                output: 'Bye'
+            }
+        ]
+
+        return this.tests;
+    }
+    
+    tests() {
+        return this.tests;
+    }
+}
+
+module.exports = TestFile;
