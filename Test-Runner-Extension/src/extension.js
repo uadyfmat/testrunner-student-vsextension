6c99f64672632895dbcb3b05be971273a72de2eb
Esteban <3tesban.madrazo@gmail.com>
2024-09-21 11:46:43 -0600

    Technical modifications

--- Test-Runner-Extension/src/extension.js
+++ Test-Runner-Extension/src/extension.js
@@ -4,52 +4,21 @@
 const vscode = require('vscode');
 const TestFile = require('./TestFile.js');
 const Test = require('./Test.js');
-const findSpecFiles = require('./Utils.js');
+const utils = require('./Utils.js');
 
 function activate(context) {
 
     // need it to run the tests in the extension
     setupTestApi(context);
     let path = vscode.workspace.workspaceFolders[0].uri.fsPath;
-    const specFilesFound = findSpecFiles(path);
+    const specFilesFound = utils.findSpecFiles(path);
     console.log(specFilesFound);
 
     // Función para instalar Test-Runner
-    let installTestRunner = vscode.commands.registerCommand('test-runner-extension-0.installTestRunner', () => {
-        const terminal = vscode.window.createTerminal({
-            name: "Terminal de Instalación",
-            shellPath: "C:\\Program Files\\Git\\bin\\bash.exe"
-        });
-        terminal.sendText("npm i -g uadyfmat/test-runner");
-        terminal.show();
-    });
+    let installTestRunner = vscode.commands.registerCommand('test-runner-extension-0.installTestRunner', utils.installExtension);
 
     // Función para ejecutar Test-Runner con entrada del usuario
-    let runTest = vscode.commands.registerCommand('test-runner-extension-0.runTest', async () => {
-        // Solicita al usuario que introduzca el código del lenguaje
-        const languageCode = await vscode.window.showInputBox({
-            prompt: "Introduce el código del lenguaje para test-runner",
-            placeHolder: "Código del lenguaje (ej. c, cpp, java, py)"
-        });
-
-        // Solicita al usuario que introduzca el nombre de la carpeta
-        const folderName = await vscode.window.showInputBox({
-            prompt: "Introduce el nombre de la carpeta para test-runner",
-            placeHolder: "Nombre de la carpeta"
-        });
-
-        if (languageCode && folderName) {
-            // Crea una terminal y ejecuta el comando con las variables
-            const terminal = vscode.window.createTerminal({
-                name: "Terminal de Instalación",
-                shellPath: "C:\\Program Files\\Git\\bin\\bash.exe"
-            });
-            terminal.sendText(`test-runner --language ${languageCode} ${folderName}`);
-            terminal.show();
-        } else {
-            vscode.window.showErrorMessage("Se necesitan el código del lenguaje y el nombre de la carpeta para ejecutar test-runner.");
-        }
-    });
+    let runTest = vscode.commands.registerCommand('test-runner-extension-0.runTest', utils.runTestRunner);
 
     // Agrega ambas funciones a las suscripciones del contexto
     context.subscriptions.push(installTestRunner);
@@ -65,6 +34,7 @@ function setupTestApi(context) {
 	let testFile = new TestFile();
 	let tests = testFile.search();
 
+	// @ts-ignore
 	let testItems = tests.map(test => controller.createTestItem(test.name, test.description));
 	for (let item of testItems) {
 		controller.items.add(item);
